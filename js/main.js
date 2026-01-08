54df54c28bf7743e006f68a94c6cc42851892e2d
AbeerIsmail <109796971+AbeerAbuKhubaiza@users.noreply.github.com>
2026-01-08 23:54:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+document.getElementById("downloadBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const fileUrl = "./images/cv.pdf";
+  const fileName = "Shahd_CV.pdf";
+
+  const link = document.createElement("a");
+  link.href = fileUrl;
+  link.download = fileName;
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
 window.addEventListener("scroll", () => {
   const nav = document.querySelector(".navbar");
   if (window.scrollY > 50) {
@@ -38,6 +53,7 @@ window.addEventListener("scroll", () => {
   });
 });
 
+
 const scrollTopBtn = document.getElementById("scrollTopBtn");
 
 window.addEventListener("scroll", function () {
@@ -53,4 +69,43 @@ scrollTopBtn.addEventListener("click", function () {
     top: 0,
     behavior: "smooth",
   });
+});
+(function () {
+  emailjs.init("eXCLePyu7ghZNFCAc");
+})();
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const btn = form.querySelector("button");
+  const oldText = btn.innerText;
+  btn.innerText = "Sending...";
+
+  emailjs.sendForm("service_hcjipuj", "template_8qnl0qg", this).then(
+    function () {
+      Swal.fire({
+        icon: "success",
+        title: "Message Sent!",
+        text: "Thank you for reaching out. We’ll get back to you shortly.",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+
+      btn.innerText = oldText;
+      form.reset();
+    },
+    function (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops!",
+        text: "Something went wrong while sending your message. Please try again.",
+        confirmButtonColor: "#d33",
+      });
+
+      console.log("FAILED...", error);
+      btn.innerText = oldText;
+    }
+  );
 });
